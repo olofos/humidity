a0f0d4b43cda694c643ffa16ffa983ae9b359a2f
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2019-10-29 20:24:00 +0100

    Restrore deleted functions

--- software/node/src/pretty-print.c
+++ software/node/src/pretty-print.c
@@ -7,6 +7,34 @@
 #include "measurement.h"
 #include "pretty-print.h"
 
+static void print_temperature(int16_t temperature)
+{
+    uint32_t t;
+    char s;
+
+    if(temperature >= 0) {
+        s = ' ';
+        t = temperature;
+    } else {
+        s = '-';
+        t = -temperature;
+    }
+
+    int integer = t  >> SHTC3_TEMPERATURE_SHIFT;
+    int fraction = (100 * (t & 0x00FF)) >> SHTC3_TEMPERATURE_SHIFT;
+
+    printf("Temperature: %c%d.%02dC\r\n", s, integer, fraction);
+}
+
+static void print_humidity(uint16_t humidity)
+{
+    uint32_t h = humidity * 100;
+    int integer = h  >> 16;
+    int fraction = (100 * (h & 0x0000FFFF)) >> 16;
+
+    printf("Humidity:     %d.%02d%%\r\n", integer, fraction);
+}
+
 void print_timestamp(const struct pkg_timestamp *timestamp)
 {
     printf("20%02X-%02X-%02X %02X:%02X:%02X\r\n",
