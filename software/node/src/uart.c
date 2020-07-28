116fdaa7e99ae666fbda474870397a2dd792b631
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2020-07-28 11:43:10 +0200

    Remove magic constants in UART driver

--- software/node/src/uart.c
+++ software/node/src/uart.c
@@ -6,6 +6,11 @@
 #include "cbuf.h"
 #include "uart.h"
 
+#define LSE_FREQ 32768
+#define BAUD 9600
+
+// LEN should be power of 2 and less than 256
+
 #define tx_cbuf_LEN 64
 
 struct tx_cbuf {
@@ -20,11 +25,11 @@ void uart_init(void)
     if(!(LPUART1->CR1 & USART_CR1_UE)) {
         RCC->APB1ENR |= RCC_APB1ENR_LPUART1EN;
 
-        LPUART1->BRR = 0x369;
+        LPUART1->BRR = 256 * (uint32_t) LSE_FREQ / BAUD;
         LPUART1->CR1 = USART_CR1_TE | USART_CR1_UE;
 
+        // DMA channel 7 remapped to LPUART1_TX
         DMA1_CSELR->CSELR = (0x05 << DMA_CSELR_C7S_Pos);
-
         DMA1_Channel7->CPAR = (uint32_t) &LPUART1->TDR;
 
         cbuf_init(tx_cbuf);
@@ -58,6 +63,7 @@ static void uart_dma_tx_start(void)
     uint32_t len = cbuf_linear_len(tx_cbuf);
 
     if(len > 0) {
+        // Use tx_cbuf.tail and not cbuf_tail(tx_cbuf) so we count mod sizeof(tail) and not mod LEN
         tx_cbuf.next_tail = tx_cbuf.tail + len;
 
         DMA1_Channel7->CMAR = (uint32_t) cbuf_tail(tx_cbuf);
@@ -76,7 +82,6 @@ ssize_t _write_r(struct _reent *reent, int fd, const void *buf, size_t count)
         if(LPUART1->CR3 & USART_CR3_DMAT) {
             size_t num_left = count;
 
-            GPIOA->BSRR = GPIO_BSRR_BS_15;
             while(num_left > 0) {
                 size_t cap = cbuf_capacity(tx_cbuf);
                 size_t num_to_write = num_left < cap ? num_left : cap;
@@ -91,7 +96,6 @@ ssize_t _write_r(struct _reent *reent, int fd, const void *buf, size_t count)
                     uart_dma_tx_start();
                 }
             }
-            GPIOA->BSRR = GPIO_BSRR_BR_15;
         } else {
             for(size_t i = 0; i < count; i++) {
                 while(!(LPUART1->ISR & USART_ISR_TXE)) {
