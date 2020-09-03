1e3efb51e44179c5c732281117b62a2053acd4e8
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2020-09-03 08:08:21 +0200

    Renamed node parameter to node_id

--- software/gateway/src/package-handler.c
+++ software/gateway/src/package-handler.c
@@ -63,7 +63,7 @@ static const char *format_time(time_t *tp)
     return s;
 }
 
-static void handle_package_register(struct pkg_buffer *p, int len, uint8_t node)
+static void handle_package_register(struct pkg_buffer *p, int len, uint8_t node_id)
 {
     uint8_t node_type = pkg_read_byte(p);
 
@@ -71,25 +71,27 @@ static void handle_package_register(struct pkg_buffer *p, int len, uint8_t node)
     uint32_t hash2 = pkg_read_dword(p);
     uint64_t hash = (hash1 << 32) | hash2;
 
-    if(db_register_node(node, node_type, hash) == DB_OK) {
+    if(db_register_node(node_id, node_type, hash) == DB_OK) {
         struct pkg_timestamp pkg_timestamp = convert_time_to_pkg_timestamp(time(0));
 
         pkg_write_byte(p, PKG_SET_TIME);
         pkg_write_timestamp(p, &pkg_timestamp);
 
         printf("New node\n");
-        printf("Node id:    %d\n", node);
+        printf("Node id:    %d\n", node_id);
         printf("Type:       %d\n", node_type);
         printf("Hash:       %" PRIx64 "\n", hash);
-    } else {
-        pkg_write_byte(p, PKG_NACK);
-        pkg_write_byte(p, 0x00);
 
-        printf("Error when registering node\n");
+        return;
     }
+
+    pkg_write_byte(p, PKG_NACK);
+    pkg_write_byte(p, 0x00);
+
+    printf("Error when registering node\n");
 }
 
-static void handle_package_measurement(struct pkg_buffer *p, int len, uint8_t node)
+static void handle_package_measurement(struct pkg_buffer *p, int len, uint8_t node_id)
 {
     struct pkg_timestamp pkg_timestamp;
     pkg_read_timestamp(p, &pkg_timestamp);
@@ -106,11 +108,11 @@ static void handle_package_measurement(struct pkg_buffer *p, int len, uint8_t no
         double vcc = ((double) vcc_raw) / (1 << ADC_VOLTAGE_SHIFT);
         double vmid = ((double) vmid_raw) / (1 << ADC_VOLTAGE_SHIFT);
 
-        int row_id = db_add_measurement(node, timestamp, humidity, temperature, vcc, vmid);
+        int row_id = db_add_measurement(node_id, timestamp, humidity, temperature, vcc, vmid);
         if(row_id >= 0) {
             uint8_t flags = 0x00;
 
-            int uptodate = db_check_firmware_is_uptodate(node);
+            int uptodate = db_check_firmware_is_uptodate(node_id);
 
             if(!uptodate) {
                 uint64_t latest_hash = db_get_latest_firmware_hash();
@@ -129,7 +131,7 @@ static void handle_package_measurement(struct pkg_buffer *p, int len, uint8_t no
                 printf("Measurement already added\n");
             } else {
                 printf("New measurement #%d\n", row_id);
-                printf("Node id:     %d\n", node);
+                printf("Node id:     %d\n", node_id);
                 printf("Timestamp:   %s\n", format_time(&timestamp));
                 printf("Temperature: %.2fC\n", temperature);
                 printf("Humidity:    %.2f%%\n", humidity);
@@ -161,7 +163,7 @@ static void handle_package_measurement(struct pkg_buffer *p, int len, uint8_t no
 }
 
 
-static void handle_package_debug(struct pkg_buffer *p, int len, uint8_t node)
+static void handle_package_debug(struct pkg_buffer *p, int len, uint8_t node_id)
 {
     struct pkg_timestamp pkg_timestamp;
     pkg_read_timestamp(p, &pkg_timestamp);
@@ -172,9 +174,9 @@ static void handle_package_debug(struct pkg_buffer *p, int len, uint8_t node)
     int msg_len = pkg_read_string(p, msg, sizeof(msg));
 
     if(pkg_timestamp.year > 0) {
-        printf("Message from %d at %s: %s\n", node, format_time(&timestamp), msg);
+        printf("Message from %d at %s: %s\n", node_id, format_time(&timestamp), msg);
 
-        int row_id = db_add_debug_message(node, timestamp, msg, msg_len);
+        int row_id = db_add_debug_message(node_id, timestamp, msg, msg_len);
         if(row_id >= 0) {
             pkg_write_byte(p, PKG_ACK);
             pkg_write_byte(p, 0x00);
@@ -184,7 +186,7 @@ static void handle_package_debug(struct pkg_buffer *p, int len, uint8_t node)
         }
     } else {
         printf("Timestamp too old: %s\n", format_time(&timestamp));
-        printf("Message from %d: \"%s\"\r\n", node, msg);
+        printf("Message from %d: \"%s\"\r\n", node_id, msg);
 
         pkg_write_byte(p, PKG_NACK);
         pkg_write_byte(p, PKG_FLAG_NOT_REGISTERED);
@@ -193,23 +195,23 @@ static void handle_package_debug(struct pkg_buffer *p, int len, uint8_t node)
 
 void handle_package(struct pkg_buffer *p, int len)
 {
-    uint8_t node = p->from;
+    uint8_t node_id = p->from;
     uint8_t request = pkg_read_byte(p);
 
     time_t receive_time = time(0);
-    printf("\r\nReceived package from %d at %s\n", node, format_time(&receive_time));
+    printf("\r\nReceived package from %d at %s\n", node_id, format_time(&receive_time));
 
     switch(request) {
     case PKG_REGISTER: // Register
-        handle_package_register(p, len, node);
+        handle_package_register(p, len, node_id);
         break;
 
     case PKG_MEASUREMENT: // Measurement
-        handle_package_measurement(p, len, node);
+        handle_package_measurement(p, len, node_id);
         break;
 
     case PKG_DEBUG:
-        handle_package_debug(p, len, node);
+        handle_package_debug(p, len, node_id);
         break;
     }
 }
